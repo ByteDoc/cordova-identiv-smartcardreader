40a3f7b631cdc304803079a4a2160ff95b328d26
Max Schaufler <bytedoc@bytedoc.net>
2019-03-26 15:02:48 +0100

    callbackContext.error added for replaced return statement

--- src/android/IdentivSmartcardReader.java
+++ src/android/IdentivSmartcardReader.java
@@ -334,6 +334,7 @@ public class IdentivSmartcardReader extends CordovaPlugin {
 			
         } catch (JSONException e) {
             Log.e("IdentivSmartcardReader", "JSONException: " + e);
+			callbackContext.error("IdentivSmartcardReader, JSONException: " + e");
 			//return true;
         }
 		
